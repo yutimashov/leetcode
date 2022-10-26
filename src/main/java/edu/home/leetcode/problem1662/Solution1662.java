57d8d83976982a66644d8c4bb857ecea031387a4
yutimashov <timashov.yuriy@gmail.com>
2022-10-26 10:23:24 +0300

    solution

---
+++ src/main/java/edu/home/leetcode/problem1662/Solution1662.java
@@ -0,0 +1,17 @@
+package edu.home.leetcode.problem1662;
+
+public class Solution1662 {
+
+    public static boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+        StringBuilder sb1 = new StringBuilder();
+        StringBuilder sb2 = new StringBuilder();
+        for (String s : word1) {
+            sb1.append(s);
+        }
+        for (String s : word2) {
+            sb2.append(s);
+        }
+        return sb1.toString().equals(sb2.toString());
+    }
+
+}
